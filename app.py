a56f0afc4c06c7646c2b1d82c121053adc64a7be
dkmatt0 <dkmatt0@gmx.fr>
2020-05-08 18:59:04 +0200

    Création des tests de pré-requis

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
+import sys
 
 
 def yml_or_yaml(filename):
@@ -13,3 +15,23 @@ def yml_or_yaml(filename):
   if os.path.isfile(filename+'.yml'): return filename+'.yml'
   if os.path.isfile(filename+'.yaml'): return filename+'.yaml'
   return None
+
+# Test si ansible est installé
+if not shutil.which('ansible-playbook'):
+  print("Vous devez d'abord installer ansible.")
+  exit(1)
+
+# Test de tous les prérequis :
+# - le fichier main.yaml existe et est accessible en lecture
+# - le dossier inventory existe est n'est pas vide
+playbook_file = yml_or_yaml('main')
+if not playbook_file:
+  print("Aucun fichier main.yml ou main.yaml n'a été trouvé.", file=sys.stderr)
+  exit(1)
+elif not os.access(playbook_file, os.R_OK):
+  print("Le fichier {} ne peut pas être lu.".format(playbook_file), file=sys.stderr)
+  exit(1)
+elif not (os.path.isdir('inventory') and os.access('inventory', os.R_OK) and os.access('inventory', os.X_OK) and len(os.listdir('inventory'))>=1):
+  exit(1)
+
+
